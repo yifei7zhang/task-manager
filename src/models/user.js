330618f14cf3bccadc4e66d64a93edb592208c04
Yifei Zhang <yifei7.zhang@gmail.com>
2020-08-29 15:19:24 -0400

    adding create endpoints

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("password cannot contain password!");
+      }
+      if (value.length <= 6) {
+        throw new Error("password length is too short!");
+      }
+    },
+  },
+});
+
+module.exports = User;

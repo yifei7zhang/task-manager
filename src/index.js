bbec2e7fe6d00d07d2f6a7a6bd09b88dc75b9524
Yifei Zhang <yifei7.zhang@gmail.com>
2020-09-06 20:02:23 -0400

    auth + middlewares

--- src/index.js
+++ src/index.js
@@ -3,14 +3,34 @@ const bodyParser = require("body-parser");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled.");
+//   } else {
+//     next();
+//   }
+// });
+
+// Maintenance middleware
+app.use((req, res, next) => {
+  res
+    .status(503)
+    .send("the site is under maintenance, please check back soon.");
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+// Middleware
+// new request ==> do something ==> run route handler
+// Can prevent route handler; do something, etc.
 
 app.get("/", (req, res) => {
   res.send("App online!");
@@ -19,3 +39,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`App is now listening on ${port}`);
 });
+
+const jwt = require("jsonwebtoken");
+const myFunction = async () => {
+  const token = jwt.sign({ _id: "abc123" }, "thisisarandomstring", {
+    expiresIn: "7 days",
+  });
+  console.log(token);
+
+  const data = jwt.verify(token, "thisisarandomstring");
+  console.log(data);
+};
+
+myFunction();
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -18,6 +20,7 @@ const User = mongoose.model("User", {
   },
   email: {
     type: String,
+    unique: true,
     trim: true,
     lowercase: true,
     required: true,
@@ -40,6 +43,45 @@ const User = mongoose.model("User", {
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisisarandomstring");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
+
+// Hashes plain text pw before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
